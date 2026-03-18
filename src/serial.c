e9a0e1e1ec84950e3caacc34e6b1945ada5d2190
Bjarke Hammerbak Paluszewski <bjarkehp@gmail.com>
2026-03-18 12:26:20 +0100

    matrix_multiply_transposed_serial header

--- src/serial.c
+++ src/serial.c
@@ -57,3 +57,13 @@ void matrix_multiply_serial(Matrix a, Matrix b, Matrix c) {
         }
     }
 }
+
+void matrix_multiply_transposed_serial(Matrix a, Matrix bT, Matrix c) {
+    for (size_t i = 0; i < c.rows; i++) {
+        for (size_t j = 0; j < c.cols; j++) {
+            for (size_t k = 0; k < a.cols; k++) {
+                *matrix_index(c, i, j) += *matrix_index(a, i, k) * *matrix_index(bT, j, k);
+            }
+        }
+    }
+}
---
+++ src/serial.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "matrix.h"
+
+void matrix_multiply_transposed_serial(Matrix a, Matrix bT, Matrix c);
