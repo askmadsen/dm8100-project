32244eca386bd101ba0c457b54130fddac1a8023
Your Name <you@example.com>
2026-04-10 11:46:34 +0200

    Updated main_openmp.c

--- src/main_openmp.c
+++ src/main_openmp.c
@@ -6,31 +6,57 @@
 #include "matrix.h"
 #include "openmp.h"
 
-int main(int argc, char **argv) {
-    if (argc != 4) {
-        fprintf(stderr, "usage: main_openmp <dimension> <scale> <num_threads> \n");
-        return 1;
+int parse_args(int argc, char** argv, int* dim, int* num_threads, char** dest) {
+    if (argc < 3) {
+        return -1;
     }
 
-    int dim   = atoi(argv[1]);
-    int scale = atoi(argv[2]);
-    int num_threads = atoi(argv[3]);
+    *dim = atoi(argv[1]);
+    *num_threads = atoi(argv[2]);
+
+    int i = 3;
+    while (i < argc) {
+        if (!strcmp(argv[i], "--dest")) {
+            i++;
+            if (i < argc) {
+                *dest = argv[i];
+            }
+            i++;
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char **argv) {
+
+    int dim;
+    int num_threads;
+    char* dest = NULL;
+
+    parse_args(argc, argv, &dim, &num_threads, &dest);
 
     omp_set_num_threads(num_threads);
 
-    struct timespec start, end;
-
-    Matrix a = matrix_filled(dim, dim, scale);
-    Matrix b = matrix_filled(dim, dim, scale * 7);
-    matrix_transpose(b);
+    Matrix a = matrix_filled(dim, dim, 7);
+    Matrix b = matrix_filled(dim, dim, 13);
     Matrix c = matrix_calloc(dim, dim);
 
+
+    struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
+
+    matrix_transpose(b);
     matmul_openmp_transposed(a, b, c);
+
     clock_gettime(CLOCK_MONOTONIC, &end);
 
     double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
     printf("%.6f\n", elapsed);
+
+    if (dest != NULL) {
+        FILE* file = fopen(dest, "w");
+        matrix_save(c, file);
+    }
 
     matrix_free(a);
     matrix_free(b);
