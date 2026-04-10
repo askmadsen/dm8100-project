30b9e114cd3a771397db2352dec3418c66fcbba4
Bjarke Hammerbak Paluszewski <bjarkehp@gmail.com>
2026-04-10 11:40:24 +0200

    arguments to main_serial.c

--- src/main_serial.c
+++ src/main_serial.c
@@ -1,33 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #include "matrix.h"
 #include "serial.h"
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
-        fprintf(stderr, "usage: main_serial <dimension> <scale>\n");
-        return 1;
+int parse_args(int argc, char** argv, int* dim, char** dest) {
+    if (argc < 2) {
+        return -1;
     }
 
-    int dim   = atoi(argv[1]);
-    int scale = atoi(argv[2]);
+    *dim = atoi(argv[1]);
 
-    struct timespec start, end;
+    int i = 2;
+    while (i < argc) {
+        if (!strcmp(argv[i], "--dest")) {
+            i++;
+            if (i < argc) {
+                *dest = argv[i];
+            }
+            i++;
+        }
+    }
 
-    Matrix a = matrix_filled(dim, dim, scale);
-    Matrix b = matrix_filled(dim, dim, scale * 7);
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    int dim;
+    char* dest = NULL;
+    parse_args(argc, argv, &dim, &dest);
+
+    Matrix a = matrix_filled(dim, dim, 7);
+    Matrix b = matrix_filled(dim, dim, 13);
     Matrix c = matrix_calloc(dim, dim);
+    
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
 
     matrix_transpose(b);
-    
-    clock_gettime(CLOCK_MONOTONIC, &start);
     matmul_transposed(a, b, c);
+
     clock_gettime(CLOCK_MONOTONIC, &end);
 
     double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
     printf("%.6f\n", elapsed);
+
+    if (dest != NULL) {
+        FILE* file = fopen(dest, "w");
+        matrix_save(c, file);
+        fclose(file);
+    }
 
     matrix_free(a);
     matrix_free(b);
