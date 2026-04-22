c9ba98e686a85e2c79d0fb9088c23f321285ccd0
Bjarke Hammerbak Paluszewski <bjarkehp@gmail.com>
2026-04-22 11:55:32 +0200

    arg_parser.h is its own file now

--- src/main_cuda.cu
+++ src/main_cuda.cu
@@ -8,14 +8,7 @@
 
 #include "matrix.h"
 #include "cuda.h"
-
-#define ARG_INIT() int i = 1;
-#define STR_ARG(var) if (argc >= i) { *var = argv[i++]; } else { return -1; }
-#define INT_ARG(var) if (argc >= i) { *var = atoi(argv[i++]); } else { return -1; }
-#define OPT_START() while (i < argc) {
-#define OPT_END() return -1; }
-#define OPT_STR_ARG(arg, var) if (!strcmp(argv[i], arg)) { i++; if (i < argc) { *var = argv[i]; } i++; } else
-#define OPT_INT_ARG(arg, var) if (!strcmp(argv[i], arg)) { i++; if (i < argc) { *var = atoi(argv[i]); } i++; } else
+#include "arg_parser.h"
 
 int parse_args(int argc, char** argv, int* dim, int* threads, int* blocks, char** dest) {
     ARG_INIT()
